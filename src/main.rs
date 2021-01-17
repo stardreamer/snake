4bfd14e444599e24b18ef0722e8a7b16c1f49a3e
Arthur Asylgareev <arthur@asylgareev.ru>
2021-01-17 14:28:25 +0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use amethyst::{
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
+
+pub struct Snake;
+
+impl SimpleState for Snake {}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let display_config_path = app_root.join("config").join("display.ron");
+
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
+            .with_plugin(RenderFlat2D::default()),
+    )?;
+
+    let assets_dir = app_root.join("assets");
+
+    let mut game = Application::new(assets_dir, Snake, game_data)?;
+
+    game.run();
+
+    Ok(())
+}
