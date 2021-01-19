9d0ddcbf4b9f151aa7dd028d95d2776055434c6c
Arthur Asylgareev <arthur@asylgareev.ru>
2021-01-19 19:17:13 +0500

    Camera intialization was added

--- src/snake.rs
+++ src/snake.rs
@@ -1,17 +1,39 @@
 use crate::ArenaConfig;
-use amethyst::{GameData, SimpleState, StateData};
-use amethyst::utils::application_root_dir;
+use amethyst::core::timing::Time;
 use amethyst::prelude::WorldExt;
+use amethyst::utils::application_root_dir;
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::transform::Transform,
+    ecs::{Component, DenseVecStorage, Entity},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    ui::{Anchor, LineMode, TtfFormat, UiText, UiTransform},
+};
+use amethyst::{GameData, SimpleState, StateData};
 
 pub struct Snake;
+
+fn initialise_camera(world: &mut World) {
+    let (arena_height, arena_width) = {
+        let config = &world.read_resource::<ArenaConfig>();
+        (config.height, config.width)
+    };
+
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(arena_width * 0.5, arena_height * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(arena_width, arena_height))
+        .with(transform)
+        .build();
+}
 
 impl SimpleState for Snake {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         let world = data.world;
 
-        let (arena_height, arena_width) = {
-            let config = &world.read_resource::<ArenaConfig>();
-            (config.height, config.width)
-        };
+        initialise_camera(world);
     }
 }
